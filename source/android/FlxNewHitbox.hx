4be344c430b79f7a9d79a91e88273a422056bfde
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-17 18:24:13 +0800

    Update FlxNewHitbox.hx

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -38,8 +38,8 @@ class FlxNewHitbox extends FlxSpriteGroup
 		add(buttonUp = createHint(FlxG.width / 2, 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0x00FF00));
 		add(buttonRight = createHint((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), Std.int(FlxG.height * 0.8), 0xFF0000));
                 add(buttonSpace = createHint(0, (FlxG.height / 5) * 4, Std.int(FlxG.width / 3), Std.int(FlxG.height / 5), 0xFFFF00));
-		add(buttonShift = createHint(FlxG.width / 3, 0, Std.int(FlxG.width / 3), Std.int(FlxG.height * 0.2), 0xFFFF00));
-		add(buttonCtrl = createHint((FlxG.width / 3) + (FlxG.width / 3), 0, Std.int(FlxG.width / 3), Std.int(FlxG.height * 0.2), 0xFFFF00));
+		add(buttonShift = createHint(FlxG.width / 3, 0, Std.int(FlxG.width / 3), Std.int(FlxG.height / 5), 0xFFFF00));
+		add(buttonCtrl = createHint((FlxG.width / 3) + (FlxG.width / 3), 0, Std.int(FlxG.width / 3), Std.int(FlxG.height / 5), 0xFFFF00));
 				
                 default:
         
